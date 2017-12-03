b77346c22160f6bca038fd565c1c0dd9c647fbca
Tatiana1111 <32689851+Tatiana1111@users.noreply.github.com>
2017-12-03 15:12:08 -0500

    Update load.sql

--- db-research/load.sql
+++ db-research/load.sql
@@ -1,4 +1,4 @@
-\COPY People(netid, first_name, last_name, email) FROM 'data/People.dat' WITH DELIMITER ',' NULL '' CSV
+\COPY People(netid, first_name, last_name, email, website, resume, password) FROM 'data/People.dat' WITH DELIMITER ',' NULL '' CSV
 \COPY Department(id, name) FROM 'data/Department.dat' WITH DELIMITER ',' NULL '' CSV
 \COPY Faculty(netid, title, opening) FROM 'data/Faculty.dat' WITH DELIMITER ',' NULL '' CSV
 \COPY Student(netid, status, start_year) FROM 'data/Student.dat' WITH DELIMITER ',' NULL '' CSV
