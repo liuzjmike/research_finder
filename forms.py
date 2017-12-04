f3c5c6548560bd0134d04fca411df056af95fd25
Mike Liu <zl99@duke.edu>
2017-12-03 22:06:37 -0500

    Have a seemingly working back-end

--- forms.py
+++ forms.py
@@ -1,65 +1,88 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField
-from wtforms.validators import DataRequired
+from wtforms import FileField, IntegerField, PasswordField, RadioField, StringField
+from wtforms.validators import DataRequired, Email, EqualTo, \
+    Optional, StopValidation, TextAreaField, URL, ValidationError
 
 
-def signup():
+class SignupForm(FlaskForm):
+    netid = StringField('NetID', [DataRequired()])
+    first_name = StringField('First Name', [DataRequired()])
+    last_name = StringField('Last Name', [DataRequired()])
+    email = StringField('Email', [DataRequired(), Email()])
+    password = PasswordField('Password', [DataRequired()])
+    confirm = PasswordField(
+        'Confirm Password', [DataRequired(),
+                             EqualTo('password', message='Passwords must match')])
+    department1 = StringField('Department 1', [DataRequired()])
+    department2 = StringField('Department 2')
+    website = StringField('Website', [Optional(), URL()])
+    resume = FileField('Resume')
+    role = RadioField(
+        'Role', [DataRequired()], choices=[('faculty', 'Faculty'), ('student', 'Student')])
+    title = StringField('Title')
+    opening = IntegerField('Opening')
+    status = StringField('Status')
+    start_year = IntegerField('Start Year')
+
+    def validate_title(form, field):  # pylint: disable=E0213
+        if form.role.data == 'faculty' and field.data not in \
+                ['Professor', 'Associate Professor', 'Assistant Professor', 'Lecturer']:
+            raise ValidationError(
+                'Title must be in [Professor, Associate Professor, Assistant Professor, Lecturer]')
+
+    def validate_opening(form, field):  # pylint: disable=E0213
+        if form.role.data == 'faculty':
+            DataRequired()(form, field)
+
+    def validate_status(form, field):  # pylint: disable=E0213
+        if form.role.data == 'student' and field.data not in \
+                ['Undergraduate', 'Master', 'PhD', 'Post-Doc']:
+            raise ValidationError(
+                'Title must be in [Undergraduate, Master, PhD, Post-Doc]')
+
+    def validate_year(form, field):  # pylint: disable=E0213
+        if form.role.data == 'student':
+            if not field.data:
+                raise StopValidation('Required Field.')
+            if field.data < 1838:
+                raise ValidationError(
+                    'Start year cannot be earlier than 1838.')
+
+
+class LoginForm(FlaskForm):
+    netid = StringField('NetID', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+
+
+def ProfileForm(person, faculty=None, student=None):
+    int_fields = [interest.field for interest in person.interests]
+
     class F(FlaskForm):
-        first_name = StringField()
-        last_name = StringField()
-        netid = StringField()
-        email = StringField()
-        password = StringField()
-        department = StringField()
-        website = StringField()
-        role = RadioField('Professor or Student?', choices=[('professor', 'Professor'), ('student', 'Student')])
-        title = StringField()
-        status = StringField()
-        start_year = StringField()
-        opening = StringField()
+        netid = StringField('NetID', [DataRequired()], default=person.netid)
+        first_name = StringField(
+            'First Name', [DataRequired()], default=person.first_name)
+        last_name = StringField(
+            'Last Name', [DataRequired()], default=person.last_name)
+        email = StringField(
+            'Email', [DataRequired(), Email()], default=person.email)
+        interests = TextAreaField('Interests', default=', '.join(int_fields))
+        department1 = StringField('Department 1', [DataRequired()])
+        department2 = StringField('Department 2')
+        website = StringField(
+            'Website', [Optional(), URL()], default=person.website)
+        resume = FileField('Resume', default=person.resume)
+
+    if faculty:
+        setattr(F, 'title', StringField('Title', default=faculty.title))
+        setattr(F, 'opening', IntegerField('Opening', default=faculty.opening))
+    if student:
+        setattr(F, 'status', StringField('Status', default=student.status))
+        setattr(F, 'start_year', IntegerField(
+            'Start Year', default=student.start_year))
     return F()
 
 
-def login():
-    class F(FlaskForm):
-        netid = StringField()
-        password = StringField()
-    return F()
-
-class ProfileEdit:
-    def form(person, isStudent):
-        class F(FlaskForm):
-            first_name = StringField(default=person.first_name)
-            last_name = StringFiled(default=person.last_name)
-            netid = StringField(default=person.netid)
-            email = StringField(default=person.email)
-            password = StringField()
-            confirmPassword = StringField()
-            if isStudent:
-                # get student object
-                student = db.session.query(models.Student)\
-                    .filter(models.Student.netid == person.netid)
-                status = StringField(default=student.status)
-                start_year = StringField(default=student.start_year)
-            else:
-                faculty = db.session.query(models.Faculty)\
-                    .filter(models.Faculty.netid == person.netid)
-                title = StringField(default=faculty.title)
-                opening = StringField(default=faculty.opening)
-
-            interests = db.session.query(models.Interest)\
-                .filter(models.Interest.netid == person.netid).all()
-            interest_list = ''
-            for field in interests:
-                interest_list += field
-                interest_list += '\n'
-            interest_text = StringField(default=interest_list)
-            return F()
-
-class Search:
-    def form():
-        class F(FlaskForm):
-            department_name = StringField()
-            interests = StringField()
-            professor_name = StringField()
-            return F()
+class SearchForm(FlaskForm):
+    department = StringField('Department')
+    faculty = StringField('Faculty')
+    interest = StringField('Interest')
