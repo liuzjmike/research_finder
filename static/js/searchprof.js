6d3e61f5653ef239f0ffad3589b881f09bac25a2
davidhzheng1996 <31869047+davidhzheng1996@users.noreply.github.com>
2017-12-10 21:47:42 -0500

    search results

--- static/js/searchprof.js
+++ static/js/searchprof.js
@@ -1,10 +1,24 @@
-$(document).ready(function() {
-    $.ajax({
-        url: '{{ url_for("search_prof") }}'
-    }).done(function (data) {
-        $('#professor').autocomplete({
-            source: data,
-            minLength: 2
-        });
-    });
+// AJAX call for autocomplete 
+$(document).ready(function(){
+	$("#professor").keyup(function(){
+		$.ajax({
+		type: "POST",
+		url: "search",
+		data:'term='+$(this).val(),
+		beforeSend: function(){
+			$("#professor").css("background","#FFF url(LoaderIcon.gif) no-repeat 165px");
+		},
+		success: function(data){
+			console.log(data)
+			$("#suggesstion-box-prof").show();
+			$("#suggesstion-box-prof").html(data);
+			$("#professor").css("background","#FFF");
+		}
+		});
+	});
+});
+//To select country name
+function selectCountry(val) {
+$("#professor").val(val);
+$("#suggesstion-box-prof").hide();
 }
